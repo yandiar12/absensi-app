a9c699e49c9e35f69e45625e2cea2a57fd6ed0e8
yandiar12 <yandiar.rohman@gmail.com>
2021-11-09 19:57:15 +0700

    cross origin

--- rest-api-absensi/src/main/java/com/yandiar/absensi/controllers/AuthController.java
+++ rest-api-absensi/src/main/java/com/yandiar/absensi/controllers/AuthController.java
@@ -22,6 +22,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,6 +37,7 @@ import io.swagger.annotations.ApiResponses;
  * @author YAR
  */
 
+@CrossOrigin
 @RestController
 @RequestMapping("/auth")
 @Api(value = "Auth API", produces = MediaType.APPLICATION_JSON_VALUE, tags = {"Auth"})
