8c17830c8a93e164b2284a122a4da49791aff7b9
yandiar12 <yandiar.rohman@gmail.com>
2021-11-13 15:29:37 +0700

    crud pegawai #3

--- rest-api-absensi/src/main/java/com/yandiar/absensi/repository/PegawaiRepository.java
+++ rest-api-absensi/src/main/java/com/yandiar/absensi/repository/PegawaiRepository.java
@@ -1,5 +1,7 @@
 package com.yandiar.absensi.repository;
 
+import java.util.List;
+
 import com.yandiar.absensi.model.entity.Pegawai;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,4 +12,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
  */
 public interface PegawaiRepository extends JpaRepository<Pegawai, Long> {
   Boolean existsByEmail(String email);
+
+  List<Pegawai> findByEmail(String email);
 }
--- rest-api-absensi/src/main/java/com/yandiar/absensi/services/PegawaiService.java
+++ rest-api-absensi/src/main/java/com/yandiar/absensi/services/PegawaiService.java
@@ -3,12 +3,16 @@ package com.yandiar.absensi.services;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 import com.yandiar.absensi.model.entity.Pegawai;
+import com.yandiar.absensi.model.entity.User;
 import com.yandiar.absensi.model.request.AddPegawaiRequest;
 import com.yandiar.absensi.model.request.SignupRequest;
+import com.yandiar.absensi.model.request.UpdatePegawaiRequest;
 import com.yandiar.absensi.model.response.Response;
 import com.yandiar.absensi.repository.PegawaiRepository;
+import com.yandiar.absensi.repository.UserRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -28,6 +32,9 @@ public class PegawaiService {
   PegawaiRepository pgwRepo;
 
   @Autowired
+  UserRepository userRepo;
+
+  @Autowired
   UserDetailsServiceImpl userService;
 
   private final SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
@@ -35,6 +42,12 @@ public class PegawaiService {
   public List<Pegawai> findAll() {
     List<Pegawai> lstData = pgwRepo.findAll();
     log.info("Pegawai find all : {}", lstData.size());
+    return lstData;
+  }
+
+  public List<Pegawai> findByEmail(String email) {
+    List<Pegawai> lstData = pgwRepo.findByEmail(email);
+    log.info("Pegawai find by email");
     return lstData;
   }
 
@@ -78,6 +91,75 @@ public class PegawaiService {
         res.setStatus(resUser.getStatus());
         res.setMessage(resUser.getMessage());
       }
+
+    } catch (Exception ex) {
+      ex.printStackTrace();
+      res.setStatus(HttpStatus.UNPROCESSABLE_ENTITY);
+      res.setMessage(ex.getLocalizedMessage());
+    }
+    return res;
+  }
+
+  public Response updatePegawai(Long id, UpdatePegawaiRequest data) {
+    Response res = new Response();
+    Pegawai pgw = new Pegawai();
+
+    Boolean pgwExists = pgwRepo.existsById(id);
+
+    if (!pgwExists) {
+      res.setStatus(HttpStatus.BAD_REQUEST);
+      res.setMessage("Id Pegawai does not exists");
+      return res;
+    }
+
+    try {
+      Date dateTglLahir = formatter.parse(data.getTgl_lahir());
+
+      pgw.setId(id);
+      pgw.setNama(data.getNama().toUpperCase());
+      pgw.setAlamat(data.getAlamat().toUpperCase());
+      pgw.setTgl_lahir(new java.sql.Date(dateTglLahir.getTime()));
+      pgw.setJabatan(data.getJabatan().toUpperCase());
+      pgw.setNohp(data.getNohp());
+      pgw.setFoto_profil(data.getFoto_profil());
+      pgwRepo.save(pgw);
+
+      res.setStatus(HttpStatus.OK);
+      res.setMessage("Pegawai successfully updated.");
+
+    } catch (Exception ex) {
+      ex.printStackTrace();
+      res.setStatus(HttpStatus.UNPROCESSABLE_ENTITY);
+      res.setMessage(ex.getLocalizedMessage());
+    }
+    return res;
+  }
+
+  public Response deletePegawai(Long id) {
+    Response res = new Response();
+
+    Optional<Pegawai> pegawai = pgwRepo.findById(id);
+
+    if (!pegawai.isPresent()) {
+      res.setStatus(HttpStatus.BAD_REQUEST);
+      res.setMessage("Id Pegawai does not exists");
+      return res;
+    }
+
+    Optional<User> user = userRepo.findByEmail(pegawai.get().getEmail());
+    if (!user.isPresent()) {
+      res.setStatus(HttpStatus.BAD_REQUEST);
+      res.setMessage("User does not exists");
+      return res;
+    }
+
+    try {
+
+      userRepo.delete(user.get());
+      pgwRepo.deleteById(id);
+
+      res.setStatus(HttpStatus.OK);
+      res.setMessage("Pegawai successfully updated.");
 
     } catch (Exception ex) {
       ex.printStackTrace();
