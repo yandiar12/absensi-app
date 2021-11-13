2f6c979e7ef0dfbbb4af396dd90591f80ff97610
yandiar12 <yandiar.rohman@gmail.com>
2021-11-13 15:30:23 +0700

    deploy crud pegawai

---
+++ rest-api-absensi/src/main/java/com/yandiar/absensi/model/entity/Absensi.java
@@ -0,0 +1,19 @@
+package com.yandiar.absensi.model.entity;
+
+// import javax.persistence.Entity;
+// import javax.persistence.Table;
+// import javax.persistence.UniqueConstraint;
+
+/**
+ * 
+ * @author YAR
+ */
+// @Entity
+// @Table(name = "presensi", 
+//         uniqueConstraints = { 
+//             @UniqueConstraint(columnNames = "email") 
+//         }
+//       )
+// public class Presensi {
+  
+// }
